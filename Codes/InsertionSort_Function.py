99f22eccb827c685a00591b2e4c1d6957fba43c8
repl.it user <replituser@example.com>
2021-02-19 00:27:24 +0000

    corrigir codigo

--- Codes/InsertionSort_Function.py
+++ Codes/InsertionSort_Function.py
@@ -1,18 +1,16 @@
 #InsertionSort
 
-A=[31, 41, 59, 26, 41, 58, 101, 0, 1]
+A=[31, 41, 59, 26, 41, 58, 101, 0, 1, 3.14]
 print(A)
 
 def InsertionSort(L,inverse=False):
   for j in range(1,len(A)):
-    selectedItem=A[j]
+    aux=A[j]
     i=j-1
-    Condition=A[i]>selectedItem
-    if inverse==True: Condition=A[i]<selectedItem
-    while i>=0 and Condition:
+    while i>=0 and (A[i]>aux)!=inverse:
       A[i+1]=A[i]
       i-=1
-    A[i+1]=selectedItem
+    A[i+1]=aux
   return L
 
 print()
